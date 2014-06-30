f10a92d83ade31bb50008dfe778d8f6b5cceef99
Giannis Giannakopoulos <giannisgiannak@gmail.com>
2014-06-30 15:56:43 +0300

    minor changes

--- central/src/main/scripts/create_datasets.sh
+++ central/src/main/scripts/create_datasets.sh
@@ -4,15 +4,16 @@
 # the datasets are tested with Swing and they are kept if 
 # they do not cause stalls to Swing.
 
-MEN_TEMP_FILE="men_${TYPE}_${SIZE}_temp.txt"
-WOMEN_TEMP_FILE="women_${TYPE}_${SIZE}_temp.txt"
-
 
 create_dataset(){
 
 SIZE=$1
 TYPE=$2
 JAR=$3
+
+
+MEN_TEMP_FILE="men_${TYPE}_${SIZE}_temp.txt"
+WOMEN_TEMP_FILE="women_${TYPE}_${SIZE}_temp.txt"
 
 java -cp $JAR gr.ntua.cslab.data.gen.${TYPE}DataGenerator $SIZE > ${MEN_TEMP_FILE}
 java -cp $JAR gr.ntua.cslab.data.gen.${TYPE}DataGenerator $SIZE > ${WOMEN_TEMP_FILE}
@@ -32,6 +33,10 @@ mkdir ${OUTPUT}/
 while [ "$ACHIEVED" -lt "$TARGET" ]; do
 create_dataset $DATA_SIZE $DATA_TYPE $JAR_PATH
 LOOP=$(java -cp $JAR_PATH gr.ntua.cslab.algorithms.Swing men.txt women.txt 0 2>/dev/null| grep loop | wc -l)
+
+MEN_TEMP_FILE="men_${DATA_TYPE}_${DATA_SIZE}_temp.txt"
+WOMEN_TEMP_FILE="women_${DATA_TYPE}_${DATA_SIZE}_temp.txt"
+
 
 if [ $LOOP -lt 1 ]; then
     let ACHIEVED=ACHIEVED+1
