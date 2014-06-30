3565e8f0e787c8cdbd2f189b72b6aea0b97f9203
Giannis Giannakopoulos <giannisgiannak@gmail.com>
2014-06-30 16:58:41 +0300

    added plot from one file script -- plain version

---
+++ central/src/main/scripts/plot_from_one_file.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# script used to draw a plot from a single file
+
+[ -z "$RESULTS_FILE" ] && echo -ne "RESULTS_FILE:\t" && read RESULTS_FILE
+[ -z "$OFFSET" ] && OFFSET=3 && echo "Using default OFFSET 3"
+
+gnuplot  -p << EOF
+set term post eps size 16.0,14.0 enhanced color font "Courier,30" linewidth 5
+set output "temp.eps"
+OFFSET=$OFFSET
+set xlabel "Number of men/women"
+if (OFFSET == 2) {set title "Number of steps vs Size"; set ylabel "Steps"}
+if (OFFSET == 3) {set title "Execution time vs Size"; set ylabel "Time (ms)"}
+if (OFFSET == 4) {set title "Regret cost vs Size"; set ylabel "Regret cost"}
+if (OFFSET == 5) {set title "Egalitarian cost vs Size"; set ylabel "Egalitarian cost"}
+if (OFFSET == 6) {set title "Sex equality cost vs Size"; set ylabel "Sex equality"}
+if (OFFSET == 7) {set title "Inequality cost vs Size"; set ylabel "Inequality cost"}
+set grid
+
+plot    '$RESULTS_FILE' using 1:OFFSET with lines title 'SMA',\
+        '$RESULTS_FILE' using 1:OFFSET+6 with lines title 'Swing',\
+        '$RESULTS_FILE' using 1:OFFSET+12 with lines title 'ESMA'
+
+EOF
